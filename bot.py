625eee616d747f47dbf83c723757858f369ae938
Subhrajit Prusty <subhrajit1997@gmail.com>
2019-01-27 21:18:12 +0530

    fix for xkcd, simplified data calls

--- bot.py
+++ bot.py
@@ -16,15 +16,16 @@ def update():
 	print("RunningBot.......")
 	print(request.get_json())
 	data = request.get_json()
-	group_data = int(data.get('message').get('chat').get('id'))
+	message = data.get('message')
+	group_data = int(message.get('chat').get('id'))
 
 	if group_data == GROUP_CHAT_ID:
 
 		print("Working>>>>")
 
-		if 'new_chat_member' in data.get('message'):
-			new_member_name = data.get('message').get('new_chat_member').get('first_name')
-			new_member_id = data.get('message').get('new_chat_member').get('id')
+		if 'new_chat_member' in message.keys():
+			new_member_name = message.get('new_chat_member').get('first_name')
+			new_member_id = message.get('new_chat_member').get('id')
 					
 			PAYLOAD = {
 			'chat_id': GROUP_CHAT_ID,
@@ -40,47 +41,61 @@ def update():
 			r = requests.post(BASE_URL+ "sendMessage", data=PAYLOAD_FOR_USER)
 			
 		
-		if 'text' in data.get('message'): 
-			if (data.get('message').get('text')== '/xkcd') or (data.get('message').get('text') == '/xkcd@Alfredcodex_bot'):
-			
-				random = randint(1, 2100)
-				i = requests.get("https://xkcd.com/"+str(random)+"/info.0.json")
-				if i.status_code == 200:
-					image = i.json()
-					url = image.get("img")
-					text = image.get("alt")
-				
+		if 'text' in message.keys():
+			if text.startswith("/"):
+				text = message.get('text')
+				[cmd, *args] = text[1:].split()
+				if (cmd == 'xkcd') or (cmd == 'xkcd@Alfredcodex_bot'):
+					if not args:
+						PAYLOAD['text'] = "Requires xkcd comic index. Example /xkcd 1001"
+						r = requests.post(BASE_URL + "sendMessage", data=PAYLOAD)
+					else:
+						if args[0].isdecimal():
+							comic = getXKCD(comic_index)
+						else:
+							comic = None
+						if comic:
+							PAYLOAD['caption'] = f"{comic['title']}\n\n{comic['alt']}"
+							PAYLOAD['photo'] = comic['url']
+							r = requests.post(BASE_URL + "sendPhoto", data=PAYLOAD)
+						else:
+							PAYLOAD['text'] = "Not a valid comic index"
+							r = requests.post(BASE_URL + "sendMessage", data=PAYLOAD)
+		
+				if (cmd =='helpme') or (cmd == 'helpme@Alfredcodex_bot'):
+					
 					PAYLOAD = {
 					'chat_id': GROUP_CHAT_ID,
-					'photo': url,
-					'caption':text 
+					'text': BOT_INTRO
 					}
 
-					r = requests.post(BASE_URL + "sendPhoto", data=PAYLOAD)
-		
-		if 'text' in data.get('message'): 
-			if (data.get('message').get('text') =='/helpme') or (data.get('message').get('text') == '/helpme@Alfredcodex_bot'):
-				
-				PAYLOAD = {
-				'chat_id': GROUP_CHAT_ID,
-				'text': BOT_INTRO
-				}
+					r = requests.post(BASE_URL + "sendMessage", data=PAYLOAD)
 
-				r = requests.post(BASE_URL + "sendMessage", data=PAYLOAD)
+				if (cmd == 'rules') or (cmd == 'rules@Alfredcodex_bot'):
+					
+					chat_id_of_request = message.get('from').get('id')
 
-		if 'text' in data.get('message'): 
-			if (data.get('message').get('text') == '/rules') or (data.get('message').get('text') == '/rules@Alfredcodex_bot'):
-				
-				chat_id_of_request = data.get('message').get('from').get('id')
+					PAYLOAD = {
+					'chat_id': chat_id_of_request,
+					'text': RULES
+					}
 
-				PAYLOAD = {
-				'chat_id': chat_id_of_request,
-				'text': RULES
-				}
-
-				r = requests.post(BASE_URL + "sendMessage", data=PAYLOAD)
+					r = requests.post(BASE_URL + "sendMessage", data=PAYLOAD)
 	
 	return "200, OK"
+
+
+
+def getXKCD(index):
+	r = requests.get(f"https://xkcd.com/{index}/info.0.json")
+	if r.status_code == 200:
+		data = r.json()
+		url = data.get('img')
+		alt = data.get('alt')
+		title = data.get('title')
+		return {'url': url, 'alt': alt, 'title': title}
+	else:
+		return None
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
