97c997476f37925d63c31270d6d4155b1c3f7147
Subhrajit Prusty <subhrajit1997@gmail.com>
2020-07-04 11:56:22 +0530

    flake8

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import os
-import json
+# import json
 import requests
 from loguru import logger
-from random import randint
+# from random import randint
 from dotenv import load_dotenv
 from pymongo import MongoClient
 from flask import Flask, request
@@ -18,161 +18,162 @@ MONGODB_URI = os.environ.get('MONGODB_URI')
 BASE_URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 GROUP_CHAT_ID = int(os.environ.get('GROUP_CHAT_ID'))
 PAYLOAD = {
-		'chat_id': GROUP_CHAT_ID,
-	}
+    'chat_id': GROUP_CHAT_ID,
+}
 
 client = MongoClient(MONGODB_URI, retryWrites=False)
 db = client.get_default_database()
 members = db.telegram_members
 
 
-@app.route("/update", methods = ["POST"])
+@app.route("/update", methods=["POST"])
 def update():
-	print("RunningBot.......")
-	print(request.get_json())
-	data = request.get_json()
-	message = data.get('message')
-	group_data = int(message.get('chat').get('id'))
-	
-	PAYLOAD = {
-		'chat_id': GROUP_CHAT_ID,
-	}
+    print("RunningBot.......")
+    print(request.get_json())
+    data = request.get_json()
+    message = data.get('message')
+    group_data = int(message.get('chat').get('id'))
 
-	logger.debug(GROUP_CHAT_ID)
-	logger.debug(group_data)
+    PAYLOAD = {
+        'chat_id': GROUP_CHAT_ID,
+    }
 
-	if group_data == GROUP_CHAT_ID:
+    logger.debug(GROUP_CHAT_ID)
+    logger.debug(group_data)
 
-		print("Working>>>>")
+    if group_data == GROUP_CHAT_ID:
 
-		if 'new_chat_member' in message.keys():
-			new_member_name = message.get('new_chat_member').get('first_name')
-			new_member_id = message.get('new_chat_member').get('id')
-			
-			PAYLOAD['text'] = f"Welcome to CODEX {new_member_name}"
+        print("Working>>>>")
 
-			PAYLOAD_FOR_USER = {
-			'chat_id': new_member_id,
-			'text': RULES
-			}
+        if 'new_chat_member' in message.keys():
+            new_member_name = message.get('new_chat_member').get('first_name')
+            new_member_id = message.get('new_chat_member').get('id')
 
-			r = requests.post(BASE_URL+ "sendMessage", data=PAYLOAD)
-			r = requests.post(BASE_URL+ "sendMessage", data=PAYLOAD_FOR_USER)
-			
-		
-		if 'text' in message.keys():
-			text = message.get('text')
-			if text.startswith("/"):
-				logger.debug(text)
-				[cmd, *args] = text[1:].split()
-				if (cmd == 'xkcd') or (cmd == 'xkcd@Alfredcodex_bot'):
-					if not args:
-						PAYLOAD['text'] = "Requires xkcd comic index. Example /xkcd 1001"
-						r = requests.post(BASE_URL + "sendMessage", data=PAYLOAD)
-					else:
-						if args[0].isdecimal():
-							comic = getXKCD(args[0])
-						else:
-							comic = None
-						if comic:
-							PAYLOAD['caption'] = f"{comic['title']}\n\n{comic['alt']}"
-							PAYLOAD['photo'] = comic['url']
-							r = requests.post(BASE_URL + "sendPhoto", data=PAYLOAD)
-						else:
-							PAYLOAD['text'] = "Not a valid comic index"
-							r = requests.post(BASE_URL + "sendMessage", data=PAYLOAD)
-		
-				elif (cmd =='helpme') or (cmd == 'helpme@Alfredcodex_bot'):
-					
-					PAYLOAD = {
-					'chat_id': GROUP_CHAT_ID,
-					'text': BOT_INTRO
-					}
+            PAYLOAD['text'] = f"Welcome to CODEX {new_member_name}"
 
-					r = requests.post(BASE_URL + "sendMessage", data=PAYLOAD)
+            PAYLOAD_FOR_USER = {
+                'chat_id': new_member_id,
+                'text': RULES
+            }
 
-				elif (cmd == 'rules') or (cmd == 'rules@Alfredcodex_bot'):
-					
-					chat_id_of_request = message.get('from').get('id')
+            requests.post(BASE_URL + "sendMessage", data=PAYLOAD)
+            requests.post(BASE_URL + "sendMessage", data=PAYLOAD_FOR_USER)
 
-					PAYLOAD = {
-					'chat_id': chat_id_of_request,
-					'text': RULES
-					}
+        if 'text' in message.keys():
+            text = message.get('text')
+            if text.startswith("/"):
+                logger.debug(text)
+                [cmd, *args] = text[1:].split()
+                if (cmd == 'xkcd') or (cmd == 'xkcd@Alfredcodex_bot'):
+                    if not args:
+                        PAYLOAD['text'] = "Requires xkcd comic index. Example /xkcd 1001"
+                        requests.post(
+                            BASE_URL + "sendMessage", data=PAYLOAD)
+                    else:
+                        if args[0].isdecimal():
+                            comic = getXKCD(args[0])
+                        else:
+                            comic = None
+                        if comic:
+                            PAYLOAD['caption'] = f"{comic['title']}\n\n{comic['alt']}"
+                            PAYLOAD['photo'] = comic['url']
+                            requests.post(
+                                BASE_URL + "sendPhoto", data=PAYLOAD)
+                        else:
+                            PAYLOAD['text'] = "Not a valid comic index"
+                            requests.post(
+                                BASE_URL + "sendMessage", data=PAYLOAD)
 
-					r = requests.post(BASE_URL + "sendMessage", data=PAYLOAD)
-				elif (cmd == 'register') or (cmd == 'register@Alfredcodex_bot'):
+                elif (cmd == 'helpme') or (cmd == 'helpme@Alfredcodex_bot'):
 
-					user_json = message.get('from')
-					chat_id_of_request = user_json.get('id')
-					logger.debug(args)
-					PAYLOAD = {
-							'chat_id': chat_id_of_request
-						}
+                    PAYLOAD = {
+                        'chat_id': GROUP_CHAT_ID,
+                        'text': BOT_INTRO
+                    }
 
-					if len(args):
-						valid_username = validate_github(args[0])
+                    requests.post(BASE_URL + "sendMessage", data=PAYLOAD)
 
-						if valid_username:
-							userdata = {"github_username" : valid_username,
-										"telegram_username" : user_json.get("username"),
-										"full_name": f"{user_json.get('first_name')} {user_json.get('last_name')}"}
+                elif (cmd == 'rules') or (cmd == 'rules@Alfredcodex_bot'):
 
-							logger.debug(userdata)
-							register = register_github(userdata)
-							if register:
-								PAYLOAD['text'] = f"Registered as {userdata['github_username']}"
-							else:
-								PAYLOAD['text'] = f"Could not register."
-						else:
-							PAYLOAD['text'] = "Invalid username. Try again."
-					else:
-						PAYLOAD['text'] = "No username given. Syntax is /register {GithubUsername}"
+                    chat_id_of_request = message.get('from').get('id')
 
-					r = requests.post(BASE_URL + 'sendMessage', data=PAYLOAD)
+                    PAYLOAD = {
+                        'chat_id': chat_id_of_request,
+                        'text': RULES
+                    }
 
-	
-	return "200, OK"
+                    requests.post(BASE_URL + "sendMessage", data=PAYLOAD)
+                elif (cmd == 'register') or (cmd == 'register@Alfredcodex_bot'):
 
+                    user_json = message.get('from')
+                    chat_id_of_request = user_json.get('id')
+                    logger.debug(args)
+                    PAYLOAD = {
+                        'chat_id': chat_id_of_request
+                    }
+
+                    if len(args):
+                        valid_username = validate_github(args[0])
+
+                        if valid_username:
+                            userdata = {
+                                "github_username": valid_username,
+                                "telegram_username": user_json.get("username"),
+                                "full_name": f"{user_json.get('first_name')} {user_json.get('last_name')}"}
+
+                            logger.debug(userdata)
+                            register = register_github(userdata)
+                            if register:
+                                PAYLOAD['text'] = f"Registered as {userdata['github_username']}"
+                            else:
+                                PAYLOAD['text'] = "Could not register."
+                        else:
+                            PAYLOAD['text'] = "Invalid username. Try again."
+                    else:
+                        PAYLOAD['text'] = "No username given. Syntax is /register {GithubUsername}"
+
+                    requests.post(BASE_URL + 'sendMessage', data=PAYLOAD)
+
+    return "200, OK"
 
 
 def getXKCD(index):
-	r = requests.get(f"https://xkcd.com/{index}/info.0.json")
-	if r.status_code == 200:
-		data = r.json()
-		url = data.get('img')
-		alt = data.get('alt')
-		title = data.get('title')
-		return {'url': url, 'alt': alt, 'title': title}
-	else:
-		return None
+    r = requests.get(f"https://xkcd.com/{index}/info.0.json")
+    if r.status_code == 200:
+        data = r.json()
+        url = data.get('img')
+        alt = data.get('alt')
+        title = data.get('title')
+        return {'url': url, 'alt': alt, 'title': title}
+    else:
+        return None
 
 
 def validate_github(github_username):
-	GITHUB_API = f'https://api.github.com/users/{github_username}'
+    GITHUB_API = f'https://api.github.com/users/{github_username}'
 
-	r = requests.get(GITHUB_API)
+    r = requests.get(GITHUB_API)
 
-	try:
-		if r.json()['login'].lower() == github_username.lower():
-			return r.json()['login']
-	except KeyError:
-		return False
+    try:
+        if r.json()['login'].lower() == github_username.lower():
+            return r.json()['login']
+    except KeyError:
+        return False
 
 
 def register_github(userdata):
-	try:
-		up = members.update_one({"github_username": userdata['github_username']}, 
-							{"$set": userdata},
-							upsert=True)
-	except Exception as e:
-		logger.error(e)
-		return False
+    try:
+        up = members.update_one({"github_username": userdata['github_username']},
+                                {"$set": userdata},
+                                upsert=True)
+    except Exception as e:
+        logger.error(e)
+        return False
 
-	return up
+    return up
 
 
 if __name__ == '__main__':
-	port = int(os.environ.get('PORT', 5000))
-	http_server = WSGIServer(('', port),app)
-	http_server.serve_forever() 
+    port = int(os.environ.get('PORT', 5000))
+    http_server = WSGIServer(('', port), app)
+    http_server.serve_forever()
--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-RULES = """Hello, register to github page https://codex-github.herokuapp.com with /register
+RULES = """Hello, please register on the github page https://codex-github.herokuapp.com with /register {GITHUBUSERNAME}
 """
 
-BOT_INTRO =  "Hello I'm Alfred the official butler of CODEX, here are somethings I can do:\n\n/helpme for Alfred's commands\n/xkcd for a bit of everthing\n/rules for CODEXs' rules"
+BOT_INTRO = "Hello I'm Alfred the official butler of CODEX, here are somethings I can do:\n\n/helpme for Alfred's commands\n/xkcd for a bit of everthing\n/rules for CODEXs' rules\n/register to register on the official website\n"
